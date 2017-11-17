428e83facabce0b998e8aca2594967c41282d4c6
STEEL06 <steel@null.net>
2017-11-16 23:50:26 -0200

    Tratadas mais algumas etapas com laços e corrigidas algumas exceções

--- calculo_metabolismo.py
+++ calculo_metabolismo.py
@@ -21,7 +21,7 @@ while ( lacoTMB <> 1 ):
    alturam = input ("Qual sua altura em ms ? ")
    alturacm = ( alturam * 100 )
    idade = input ("Qual sua idade em anos ? ")
-   BF = input ("Qual o seu percentual de gordura corporal? Digite 0 caso nao saiba? ")
+   BF = input ("Qual o seu percentual de gordura corporal? Digite 0 caso nao saiba. ")
 
    print ""
 
@@ -35,6 +35,7 @@ while ( lacoTMB <> 1 ):
    print "o seu peso e", peso, "kgs"
    print "a sua altura e", alturam, "ms"
    print "a sua idade e", idade, "anos"
+
    if   BF == 0:
       print "o seu percentual de gordura nao e conhecido"
    else:
@@ -42,73 +43,149 @@ while ( lacoTMB <> 1 ):
 
    print""
 
+   if   BF == 0:
+      if   genero == 1:
+         TMB = ((10 * peso) + (6.25 * alturacm) - (5 * idade) + 5  )
+      elif genero == 2:
+         TMB = ((10 * peso) + (6.25 * alturacm) - (5 * idade) - 161)
+      else:
+         print ""
+   else:
+      TMB = (370 + ( 21.6 * ( peso - (peso * BF * 1/100 ))))
+
+   print "a sua Taxa Metabolica Basal e", TMB, "kcal"
+   print ""
+
    lacoTMB = input ("Esses dados estao corretos? 1-Sim ; 2-Nao ? ")
 
+print ""
+print ""
 
-if   BF == 0:
-   if   genero == 1:
-      TMB = ((10 * peso) + (6.25 * alturacm) - (5 * idade) + 5  )
-   elif genero == 2:
-      TMB = ((10 * peso) + (6.25 * alturacm) - (5 * idade) - 161)
+
+
+print "Calculo da ICR (Ingestao Calorica Recomendada)"
+
+
+lacoICR = 0
+
+while ( lacoICR <> 1 ):
+
+   print ""
+   print "Com qual item dessa tabela se assemelha sua quantidade de exercicios semanal ?"
+   print ""
+   print "1- Nenhum ou pouco"
+   print "2- Leve, de 1 a 3 vezes por semana"
+   print "3- Moderado, de 3 a 5 vezes por semana"
+   print "4- Pesado, de 6 a 7 vezes por semana"
+   print "5- Extremo, duas vezes ao dia"
+   print ""
+
+   QES = input ("")  # QE = Quantidade de Exercicio Semanal
+   print ""
+
+   while (QES <> ( 1 or 2 or 3 or 4 or 5 )):
+
+      print "Valor invalido, escolha novamente"
+
+      print ""
+      print "Com qual item dessa tabela se assemelha sua quantidade de exercicios semanal ?"
+      print ""
+      print "1- Nenhum ou pouco"
+      print "2- Leve, de 1 a 3 vezes por semana"
+      print "3- Moderado, de 3 a 5 vezes por semana"
+      print "4- Pesado, de 5 a 7 vezes por semana"
+      print "5- Extremo, mais de uma vez ao dia"
+      print ""
+      QES = input ("")
+      print ""
+
+   if   QES == 1:
+      ICR = (1.2   * TMB)
+      print "Voce nao faz ou faz pouquissimo exercicio"
+
+   elif QES == 2:
+      ICR = (1.375 * TMB)
+      print "Voce se exercita de forma leve, de 1 a 3 vezes na semana"
+
+   elif QES == 3:
+      ICR = (1.55  * TMB)
+      print "Voce se exercita de forma moderada, de 3 a 5 vezes na semana"
+
+   elif QES == 4:
+      ICR = (1.725 * TMB)
+      print "Voce se exercita de forma pesada, de 5 a 7 vezes na semana"
+
+   elif QES == 5:
+      ICR = (1.9   * TMB)
+      print "Voce se exercita de forma extrema, mais de uma vez ao dia"
+
    else:
       print ""
-else:
-   TMB = (370 + ( 21.6 * ( peso - (peso * BF * 1/100 ))))
+
+   print ""
+   print "sua Ingestao Calorica Recomendada e de", ICR, "kcal"
+   print ""
+
+   lacoICR = input ("Esses dados estao corretos? 1-Sim ; 2-Nao ? ")
+
+print""
 
 
 
-# Calculo do ICR (Ingestao Calorica Recomendada)
+print "Calculo do balanco do Deficit ou Superavit calorico (DSC)"
 
-print " "
-print "Com qual item dessa tabela se assemelha sua quantidade de exercicios semanal ?"
-print " "
-print "1- Nenhum ou pouco"
-print "2- Leve, de 1 a 3 vezes por semana"
-print "3- Moderado, de 3 a 5 vezes por semana"
-print "4- Pesado, de 6 a 7 vezes por semana"
-print "5- Extremo, duas vezes ao dia"
-print " "
+lacoDSC = 0
 
-QES = input ("")  # QE = Quantidade de Exercicio Semanal
+while ( lacoDSC <> 1 ):
 
-if   QES == 1:
-       ICR = (1.2   * TMB)
-elif QES == 2:
-       ICR = (1.375 * TMB)
-elif QES == 3:
-       ICR = (1.55  * TMB)
-elif QES == 4:
-       ICR = (1.725 * TMB)
-elif QES == 5:
-       ICR = (1.9   * TMB)
-else:
-       print "Escolha um valor da tabela"
+   print ""
+   print "Escolha o tipo de dieta pretendida:"
+   print "1- Hipocalorica ; 2- Normocalorica ; 3- Hipercalorica"
+   print ""
+
+   DSC = input ("") # DSC: Deficit ou Superavit Calorico / CD: Calorias Diarias
 
 
+   while ( DSC <> ( 1 or 2 or 3 )):
+      print "escolha um valor possivel"
+      print ""
+      print "Escolha o tipo de dieta pretendida:"
+      print "1- Hipocalorica ; 2- Normocalorica ; 3- Hipercalorica"
+      print ""
+      DSC = input ("")
 
-# Calculo do Deficit ou Superavit calorico (DSC)
+   print ""
 
-print " "
-print "Escolha o tipo de dieta pretendida:"
-print "1- Hipocalorica ; 2- Normocalorica ; 3- Hipercalorica"
-print ""
+   if   DSC == 1:
+      kcalsem = input ("Quantos kilos pretende perder por semana? ")
+      kcaldia = ( kcalsem * 7700 / 7 )
+      CD = (ICR - kcaldia)
+      print ""
+      print "Voce optou em fazer uma dieta Hipocalorica"
+      print "Voce deseja perder", kcalsem, "kg por semana"
 
-DSC = input ("") # DSC: Deficit ou Superavit Calorico / CD: Calorias Diarias
+   elif DSC == 2:
+      CD = ICR
+      print ""
+      print "Voce optou em fazer uma dieta Normocalorica"
+      print "Voce deseja manter seu peso"
 
-print ""
+   elif DSC == 3:
+      kcalsem = input ("Quantos kilos pretende ganhar por semana? ")
+      kcaldia = ( kcalsem * 7700 / 7 )
+      CD = (ICR + kcaldia)
+      print ""
+      print "Voce optou em fazer uma dieta Hipercalorica"
+      print "Voce deseja ganhar", kcalsem, "kg por semana"
 
-if   DSC == 1:
-       kcalsem = input ("Quantos kilos pretende perder por semana? ")
-       kcaldia = ( kcalsem * 7700 / 7 )
-       CD = (ICR - kcaldia)
-elif DSC == 2:
-       CD = ICR
-elif DSC == 3:
-       kcalsem = input ("Quantos kilos pretende ganhar por semana? ")
-       kcaldia = ( kcalsem * 7700 / 7 )
-       CD = (ICR + kcaldia)
-else:
-       print "Escolha um numero valido"
+   else:
+      print ""
+
+
+   print "Sua necessidade Calorica Diaria e", CD, "kcal"
+   print ""
+
+   lacoDSC = input ("Esses dados estao corretos? 1-Sim ; 2-Nao ? ")
 
 
 
